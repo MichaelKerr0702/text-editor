c0f4ddc7fe766f41920ace647eed9cebe72cd366
MichaelKerr0702 <110789076+MichaelKerr0702@users.noreply.github.com>
2023-01-29 21:02:58 -0800

    finished

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,35 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+export const putDb = async (content) => {
+  if (err){
+    console.error('putDb not implemented')
+  }
+  const jatedb = await openDB('jate', 1);
+  const tx = jatedb.transaction('jate', 'readwrite');
+  const store= tx.objectStore('jate');
+  const request= store.put({id: id, jate: content});
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  if(err){
+    console.error('getDb not implemented')
+  }
+  const jatedb = await openDB('jate', 1);
+  const tx = jatedb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+
+}
+  
+  ;
 
 initdb();
